37e4d8a95c230e069a8765bad8393cfc241c7532
Kavyashree Arun <kavyashree.arun@gmail.com>
2022-10-10 00:04:38 +0530

    landing page

---
+++ index.js
@@ -0,0 +1,35 @@
+var speed = 200;
+var speed2 = 170;
+var str = document.getElementById('str');
+var i = 0;
+var isRemoving = false;
+
+var messages = [
+    "Writer.",
+    "Marketer."
+]
+
+function action() {
+  if (isRemoving) {
+    if (str.innerText.length > 0) {
+      str.innerText = str.innerText.substr(0, str.innerHTML.length - 1);
+      setTimeout( action, speed2 );
+      return;
+    }
+    isRemoving = false;
+    i++;
+    if (i >= messages.length) {
+      i = 0;
+    }
+    setTimeout( action, speed );
+    return;
+  }
+  var message = messages[i];
+  str.innerText = message.substr(0, str.innerHTML.length + 1);
+  if (str.innerText.length === message.length) {
+    isRemoving = true;
+  }
+  setTimeout( action, isRemoving ? speed2 : speed );
+}
+
+setTimeout( action, speed ) ;
